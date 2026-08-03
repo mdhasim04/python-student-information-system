7f311f2be9633b9118ee043ab2332e97848d6676
Md_Hasim_Hussain <mdhasimhussain2004@gmail.com>
2026-08-04 01:40:12 +0530

    Day 1: Add Student Information System project

---
+++ student_information.py
@@ -0,0 +1,12 @@
+# ========== STUDENT INFORMATION ==========
+# Student Information System - Mini Project 1
+
+print("=== Student Information System ===")
+student_name = input("Enter student name: ")
+student_age = input("Enter student age: ")
+college_name = input("Enter college name: ")
+course_name = input("Enter course name: ")
+city_name = input("Enter city name: ")
+state_name = input("Enter state name: ")
+
+print("=========================================")
